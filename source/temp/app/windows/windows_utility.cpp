f7591e76138724ac608d07660f4a51493b4afc2b
tsukushibito <tsukushibito@gmail.com>
2019-03-11 17:04:19 +0900

    Add windows_utility.cpp

---
+++ source/temp/app/windows/windows_utility.cpp
@@ -0,0 +1,21 @@
+﻿#include "temp/core/define.h"
+#ifdef TEMP_PLATFORM_WINDOWS
+#include "Windows.h"
+#include "temp/app/utility.h"
+namespace temp {
+namespace app {
+WindowViewSize GetWindowViewSize(const void* window) {
+  auto hwnd = (const HWND)(window);
+  RECT rect;
+  auto result = GetClientRect(hwnd, &rect);
+  if (result == FALSE) {
+    return WindowViewSize();
+  }
+
+  return WindowViewSize{(std::uint32_t)(rect.right - rect.left),
+                        (std::uint32_t)(rect.bottom - rect.top)};
+}
+}  // namespace app
+}  // namespace temp
+
+#endif
